04dac50fd8938e6cbf576104d42286904e2d514f
zengyu <zeng.yu.1993@gmail.com>
2021-02-20 23:42:27 +0800

    [ADD] load config vai go.ini

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.16
 
 require (
 	github.com/gin-gonic/gin v1.6.3
+	github.com/go-ini/ini v1.62.0
 	github.com/smartystreets/goconvey v1.6.4
+	gopkg.in/ini.v1 v1.62.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,14 +1,30 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-ini/ini"
+	"github.com/zmarsarc/wechat/wechat"
+)
 
 func main() {
-	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
+	config, err := ini.Load("conf/app.conf")
+	if err != nil {
+		panic(fmt.Errorf("load config error: %s", err.Error()))
+	}
+
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "ok",
 		})
 	})
 
-	router.Run()
+	wechatRouter := r.Group("wechat")
+	wechatRouter.Use(wechat.Auth(config.Section("wechat").Key("token").String()))
+	{
+	}
+
+	r.Run()
 }
